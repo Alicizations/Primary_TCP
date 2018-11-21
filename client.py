1e83b78a005f8f431cea6e2729a0d017e4983059
Alicizations <673386754@qq.com>
2018-11-21 16:40:14 +0800

    client v0.75

--- client.py
+++ client.py
@@ -1,13 +1,54 @@
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+IP_PORT = ('192.168.199.111', 5005)
+BUFSIZE = 1024
+UDP_Client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+Header = [0 for x in range(0,20)]
+Seq = 0
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+f = open('./data/lena.jpg', 'rb') #打开文件
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+seg = f.read(200)
+time = 1
+
+while len(seg):
+    packetHeader = Header.copy()
+    packetData = seg
+    seg = f.read(200)
+    if len(seg) == 0:
+        packetHeader[14] = 1
+    packetHeader[7] = Seq % 256;
+    packetHeader[6] = (Seq >> 8) % 256;
+    packetHeader[5] = (Seq >> 16) % 256;
+    packetHeader[4] = (Seq >> 24);
+    Seq += 200;
+    packet = bytes(packetHeader) + packetData
+    UDP_Client.sendto(packet, IP_PORT)
+    back_msg, addr = UDP_Client.recvfrom(BUFSIZE)
+    print(back_msg, addr)
+
+f.close() #关闭文件
+
+
+
+# def SetSourcePort(header):
+#     pass
+
+# def SetTargetPort(header):
+#     pass
+
+# def SetIndex(header):
+#     pass
+
+# def SetConfirmNum(header):
+#     pass
+
+# def SetACK(header):
+#     pass
+
+# while True:
+#     msg = input('>>: ').strip()
+#     if not msg:
+#         continue
+
+#     UDP_Client.sendto(a, IP_PORT)
